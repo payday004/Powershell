a2fbb25da2f6901a68ff28dbbde33b9dd37806f8
Peyton Dexter <60048007+payday004@users.noreply.github.com>
2021-06-03 15:28:51 -0400

    Add files via upload

---
+++ azure_ad_user_report.ps1
@@ -0,0 +1,9 @@
+﻿$path = "./azure_user_export.csv"
+
+Connect-AzureAD
+
+Get-AzureADUser -All $true | select DisplayName, Department, JobTitle, @{Label="ManagerName"; Expression={Get-AzureADUserManager -ObjectId $_.ObjectId | select -ExpandProperty DisplayName}} | Export-Csv -Path $path 
+
+Disconnect-AzureAD
+
+
